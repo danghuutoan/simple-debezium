623484aded1f851e38b1be89ac9ed2685d4ea0b3
danghuutoan <danghuutoan2002@gmail.com>
2021-12-24 23:47:31 +0700

    adding create event trigger

--- create_trigger_event.sql
+++ create_trigger_event.sql
@@ -12,38 +12,29 @@ DECLARE audit_query TEXT;
 r RECORD;
 create_citus_table text;
 BEGIN 
-IF tg_tag = 'CREATE TABLE' THEN 
-    FOR r IN SELECT * FROM pg_event_trigger_ddl_commands() LOOP 
-        RAISE NOTICE 'table % %', r.object_identity, r.command_tag;
-        if r.command_tag = 'CREATE TABLE' then
-            if get_table_name_without_hash_key(r.object_identity, 'public') = r.object_identity then
-                if left(r.object_identity, 11) in ('public.eag_', 'public."eag') then
-                BEGIN
-                perform master_get_table_metadata(get_table_name_without_hash_key(r.object_identity, 'public'));
-                RAISE NOTICE 'table is distributed already';
-                EXCEPTION WHEN OTHERS THEN
-                    RAISE NOTICE 'table is not distributed ';
-                    create_citus_table = get_table_name_without_hash_key(r.object_identity, 'public');
-                END;
-                end if;
-            else
-                BEGIN
-                perform master_get_table_metadata(get_table_name_without_hash_key(r.object_identity, 'public'));
-                RAISE NOTICE 'table is distributed already';
-                EXCEPTION WHEN OTHERS THEN
-                    RAISE NOTICE 'table is not distributed ';
-                    create_citus_table = r.object_identity;
-                END;
-            end if;
-
-        end if;
-        if r.command_tag = 'CREATE INDEX' then
-            if create_citus_table is not null then
-            RAISE NOTICE ' create distributed table for %', create_citus_table;
-            PERFORM create_distributed_table(create_citus_table, 'entity_id');
-            end if;
-        end if;
-    END LOOP;
+IF tg_tag = 'CREATE TABLE' THEN FOR r IN
+SELECT *
+FROM pg_event_trigger_ddl_commands() LOOP 
+RAISE NOTICE 'table % %', r.object_identity, r.command_tag;
+	if r.command_tag = 'CREATE TABLE' then
+		if exists(SELECT * FROM citus_shards where shard_name = r.object_identity) then
+			RAISE NOTICE 'distributed table % ', r.object_identity;
+		else
+			RAISE NOTICE 'not a distributed table % ', r.object_identity;
+			if left(r.object_identity, 11) in ('public.eag_', 'public."eag') then
+				create_citus_table = r.object_identity;
+			end if;
+		end if;
+	end if;
+	
+	if r.command_tag = 'CREATE INDEX' then
+		if create_citus_table is not null then
+			RAISE NOTICE ' create distributed table for %', create_citus_table;
+			PERFORM create_distributed_table(create_citus_table, 'entity_id');
+		end if;
+	end if;
+END LOOP;
+RAISE NOTICE ' end loop';
 END IF;
 END;
 $BODY$;
