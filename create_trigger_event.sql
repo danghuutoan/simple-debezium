ac05702b623ff47c809793875cd4a8ea4adde603
danghuutoan <danghuutoan2002@gmail.com>
2021-12-24 17:35:04 +0700

    adding create event trigger

--- create_trigger_event.sql
+++ create_trigger_event.sql
@@ -3,7 +3,7 @@
 CREATE OR REPLACE FUNCTION public.log_ddl() RETURNS event_trigger LANGUAGE 'plpgsql' COST 100 VOLATILE NOT LEAKPROOF AS $BODY$
 DECLARE audit_query TEXT;
 r RECORD;
-create_citus_table text;
+citus_table_name text;
 BEGIN IF tg_tag = 'CREATE TABLE' THEN FOR r IN
 SELECT *
 FROM pg_event_trigger_ddl_commands() LOOP RAISE NOTICE 'table % %',
@@ -15,7 +15,7 @@ if r.command_tag = 'CREATE TABLE' then if get_table_name_without_hash_key(r.obje
 RAISE NOTICE 'table is distributed already';
 EXCEPTION
 WHEN OTHERS THEN RAISE NOTICE 'table is not distributed ';
-create_citus_table = get_table_name_without_hash_key(r.object_identity, 'public');
+citus_table_name = get_table_name_without_hash_key(r.object_identity, 'public');
 END;
 end if;
 else BEGIN perform master_get_table_metadata(
@@ -24,13 +24,13 @@ else BEGIN perform master_get_table_metadata(
 RAISE NOTICE 'table is distributed already';
 EXCEPTION
 WHEN OTHERS THEN RAISE NOTICE 'table is not distributed ';
-create_citus_table = r.object_identity;
+citus_table_name = r.object_identity;
 END;
 end if;
 end if;
-if r.command_tag = 'CREATE INDEX' then if create_citus_table is not null then RAISE NOTICE ' create distributed table for %',
-create_citus_table;
-PERFORM create_distributed_table(create_citus_table, 'entity_id');
+if r.command_tag = 'CREATE INDEX' then if citus_table_name is not null then RAISE NOTICE ' create distributed table for %',
+citus_table_name;
+PERFORM create_distributed_table(citus_table_name, 'entity_id');
 end if;
 end if;
 END LOOP;
